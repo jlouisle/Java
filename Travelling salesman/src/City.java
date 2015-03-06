c537f5ea13d740cc9cde868b5e77037120c7ac09
lehlouisle <lehlouisle@gmail.com>
2015-03-06 03:02:47 +0100

    145623

---
+++ Travelling salesman/src/City.java
@@ -0,0 +1,16 @@
+
+public class City {
+	private String Name;
+	private int population;
+	private int education;
+	private String industry;
+	private String cityCouncil;
+	private boolean visited = false;
+	public City(String name, int population, int education) {
+		super();
+		Name = name;
+		this.population = population;
+		this.education = education;
+	}
+
+}
