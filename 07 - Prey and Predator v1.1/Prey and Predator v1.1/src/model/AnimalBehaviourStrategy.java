6d61c316f057a6e2e57630f87eba2271a9cfa688
lehlouisle <lehlouisle@gmail.com>
2015-03-08 16:06:54 +0100

    45

--- 07 - Prey and Predator v1.1/Prey and Predator v1.1/src/model/AnimalBehaviourStrategy.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public interface AnimalBehaviourStrategy {
-	Animal[] reproduce(Animal partner); // Give back an array of children of these two animals
-	void move(); // Move the animal
-	int getMaxAge();
-	void setAnimal(Animal animal);
-}
